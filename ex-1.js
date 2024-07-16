3797d27681a93f1efbba0272e020da1e3defddca
TiTY <tee25450@gmail.com>
2024-07-16 15:38:45 +0700

    fix: correct ex-1

--- ex-1.js
+++ ex-1.js
@@ -1,7 +1,7 @@
 function twoSum(numbers, target) {
   // เริ่มเขียนโค้ดตรงนี้จ้า
   for (let i = 0; i <= numbers.length; i++) {
-    for (let j = 0; j <= numbers.length; j++) {
+    for (let j = i + 1; j <= numbers.length; j++) {
       if (numbers[i] + numbers[j] == target) {
         return [i, j];
       }
@@ -15,5 +15,5 @@ const target1 = 9;
 console.log(twoSum(numbers1, target1));
 
 const numbers2 = [2, 7, 11, 15];
-const target2 = 23;
+const target2 = 14;
 console.log(twoSum(numbers2, target2));
